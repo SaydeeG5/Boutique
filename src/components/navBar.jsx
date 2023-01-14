15d1604cb85741fa590d7fa53d147c8e147fa0f0
Maria Saydee Guevarra <saydeeguevarra@gmail.com>
2023-01-14 15:44:32 -0800

    deleted nav bar toggler

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -10,9 +10,6 @@ function NavBar() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
         <Link className="navbar-brand" to="/home">Boutique</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                     <Link className="nav-link" aria-current="page" to="/home">Home</Link>
